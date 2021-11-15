3a29f53a3dab1c4a02f16ef466195eab36318d61
sbin0217 <94364568+sbin0217@users.noreply.github.com>
2021-11-15 20:18:18 +0900

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,25 @@
+ $(function(){
+
+        //1번째 이미지
+        $('.btn1').click(function(){
+          window.open("img/1.jpg","blank","width=1024, height=768")
+        });
+
+
+
+        //2번째 이미지
+        $('.btn2').click(function(){
+          window.open("img/6.jpg","blank","width=768, height=1024")
+        });
+
+
+
+
+        //3번째 이미지
+        $('.btn3').click(function(){
+          window.open("img/8.jpg","blank","width=768, height=1024")
+        });
+
+
+
+      });
